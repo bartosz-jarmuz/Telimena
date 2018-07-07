f8107811a62d5a452cfc3b0890f5c971b71aeddc
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-07-07 14:28:26 +0200

    make properties required

--- Telimena/Telimena.WebApp/Models/Account/LoginViewModel.cs
+++ Telimena/Telimena.WebApp/Models/Account/LoginViewModel.cs
@@ -5,9 +5,13 @@ using System.Web;
 
 namespace Telimena.WebApp.Models.Account
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class LoginViewModel
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
 
