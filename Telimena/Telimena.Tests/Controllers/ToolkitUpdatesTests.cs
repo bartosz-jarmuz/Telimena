56234c7a3995493b74ad8962defb860943b787fc
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-09-12 00:26:52 +0200

    Add some notes into tests to be done

---
+++ Telimena/Telimena.Tests/Controllers/ToolkitUpdatesTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace Telimena.Tests
+{
+    [TestFixture]
+    public class ToolkitUpdatesTests
+    {
+        //The toolkit can evolve independently of the client app - some changes might be breaking the contracts, but most - should not
+        //a non-breaking change example - add new functions or overloads, pull more client data or handle changes in the web api
+        //a breaking change would be renaming methods, changing the default URI etc - these are the things that would break the app if DLL is replaced
+        //in such cases, the app needs to be recompiled against latest toolkit.
+
+        [Test]
+        public void Test_SimpleScenario()
+        {
+            //Todo - an update request comes from a program which uses toolkit 1.0
+            //there were several changes of the toolkit since that time - 1.1, 1.2, 1.3
+            //none of them is marked as 'introduces breaking changes'
+            //regardless whether there is an app update or not, we can prompt that an update is available, and download the toolkit 1.3
+        }
+
+        [Test]
+        public void Test_ComplexScenario()
+        {
+            //how is tookit developed & updated?
+            //when the changes are made to Telimena.Client, they will be pushed to Nuget for devs to use
+            //however, the .dll should also be uploaded to Telimena.Portal,
+            //because the client applications might use new version without recompilation (& without original developer involvement!)
+
+            //Todo - an update request comes from a program FOOBARER v.4.0 which uses toolkit v1.0
+            //there were several changes of the toolkit since that time - 1.1, 1.2, 1.3 and so on up to 2.0
+            //1.4 was marked as 'introducing breaking changes'
+            //each of the program & update packages needs to have a version of toolkit it was compiled against stored in DB
+            //the FOOBARER v.4.0 was compiled against toolkit 1.0
+            //(we get that info by, either prompting the dev when package is uploaded (MVP), or verifying automatically)
+            // based on the request we can figure out that we can grab new toolkit v.1.3 (because 1.4 is breaking)
+
+            //todo - meanwhile, when a new toolkit version is uploaded and is marked as 'breaking changes'
+            //all the programs which were not compiled against that version should get a notification:
+            // - at developer portal, a box saying '4 of your apps need to be recompiled to use latest Telimena
+            //The dev should be able to open project in VS, update nuget and recompile
+            //then go to portal and upload update package - FOOBARER v.4.1
+            //When uploading an update, dev is prompted for version of toolkit that was used (later this becomes automatic)
+
+            //next time an update request comes from a program FOOBARER v.4.0 which uses toolkit v1.0 (or 1.3 at this point)
+            //it will recognize that there is an incompatible update for the toolkit, because v.4.0 is not compatible with toolkit 1.4,
+            //BUT ALSO an update for the app 
+            //the update of the application will contain the latest compatible Toolkit, based on what toolkit was the update compiled against
+            //TBD - should the toolkit come from the update package or separately?
+
+            //todo - seems like no PK/FK relation needed for ToolkitData, ToolkitPackage, UpdaterPackage - it should be handled by querying the db for compatible resources
+            // potential methods to create
+            // .GetLatestCompatibleToolkit(programVersion)
+            // .GetLatestCompatibleUpdater(updaterVersion)
+        }
+
+
+    }
+}
---
+++ Telimena/Telimena.Tests/Controllers/UpdaterUpdatesTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DbIntegrationTestHelpers;
+using Moq;
+using NUnit.Framework;
+using Telimena.Client;
+using Telimena.WebApp.Controllers.Api;
+using Telimena.WebApp.Core.Models;
+using Telimena.WebApp.Infrastructure.Database;
+using Telimena.WebApp.Infrastructure.Repository;
+using Telimena.WebApp.Infrastructure.UnitOfWork;
+
+namespace Telimena.Tests
+{
+    [TestFixture]
+    public class UpdaterUpdatesTests
+    {
+        //The updater  can evolve independently of the Toolkit - some changes might be breaking the contracts, but most - should not
+        //there were several updater updates (maybe changing UI?)
+        //first two have no breaking changes - compatible with toolkit 0.0
+        //3rd has breaking changes and requires toolkit 0.5 or above
+        //4th has breaking changes and requires toolkit 0.9 or above
+        //5th has no breaking changes, but is newer than 4th, so requires 0.9 or above
+        //6th has no breaking changes, but is newer than 4th & 5th, so requires 0.9 or above
+        //7th has breaking changes and requires toolkit 1.3 or above
+        //8th has no breaking changes, but is newer than 7th so requires 1.3 or above
+
+        //todo - When new updater is uploaded, it should specify what is the MINIMUM version of tookit that can use it
+        //First updater will have version 0.0.0.0
+        //It should be enforced automatically (OR SHOULD IT NOT?)
+        //that if previous version of an updater introduced breaking changes on a certain toolkit version
+        //then subsequent updaters should require at least that version
+        // OR MAYBE IT SHOULD BE OVERRIDABLE? (consider fixing breaking changes?)
+        // In any case, the decision whether an update should be downloaded & installed should be made in the web app
+        // The Telimena.Client and the Updater should be 100% dumb and just download whatever is returned to them.
+
+        //todo - how to automatically check if an updater is compatible with certain toolkit versions?
+
+        [Test]
+        public void Test_LatestUpdaterIsCompatible()
+        {
+            //todo - a request for update is made from toolkit v1.0
+            //there were several updates of the updater since that time
+            //however no breaking changes
+            //assert that the latest version of updater is returned
+        }
+
+        [Test]
+        public void Test_LatestUpdaterIsNotCompatible_BreakingChanges()
+        {
+            //assert that the latest version of updater is returned
+            //todo - assert that 2nd is returned for request from toolkit 0.4 
+            //todo - assert that 8th is returned for request from toolkit 2.0
+            //todo - assert that 6th is returned for request from toolkit 1.0
+        }
+    }
+}
