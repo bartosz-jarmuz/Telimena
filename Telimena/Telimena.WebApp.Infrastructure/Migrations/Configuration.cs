b0746e8c316128433a9a5b9dcf1c86fc2e84a11a
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-10-02 16:04:00 +0200

    disable config class

--- Telimena/Telimena.WebApp.Infrastructure/Migrations/Configuration.cs
+++ Telimena/Telimena.WebApp.Infrastructure/Migrations/Configuration.cs
@@ -7,21 +7,21 @@ namespace Telimena.WebApp.Infrastructure.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    public sealed class Configuration : DbMigrationsConfiguration<TelimenaContext>
-    {
-        public Configuration()
-        {
-            this.AutomaticMigrationsEnabled = false;
-            this.ContextKey = "Telimena.WebApp.Infrastructure.Database.TelimenaContext";
-        }
+    //public sealed class Configuration : DbMigrationsConfiguration<TelimenaContext>
+    //{
+    //    public Configuration()
+    //    {
+    //        this.AutomaticMigrationsEnabled = false;
+    //        this.ContextKey = "Telimena.WebApp.Infrastructure.Database.TelimenaContext";
+    //    }
 
-        protected override void Seed(TelimenaContext context)
-        {
-            TelimenaDbInitializer.SeedUsers(context);
-            TelimenaDbInitializer.SeedToolkit(context);
-            context.SaveChanges();
-        }
+    //    protected override void Seed(TelimenaContext context)
+    //    {
+    //        TelimenaDbInitializer.SeedUsers(context);
+    //        TelimenaDbInitializer.SeedToolkit(context);
+    //        context.SaveChanges();
+    //    }
 
 
-    }
+    //}
 }
