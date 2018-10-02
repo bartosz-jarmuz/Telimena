e347b2d7642cfee43f637c0b28474fffdcd2b48b
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-10-02 15:00:41 +0200

    chage initilizer

--- Telimena/Telimena.WebApp.Infrastructure/Database/TelimenaDbInitializer.cs
+++ Telimena/Telimena.WebApp.Infrastructure/Database/TelimenaDbInitializer.cs
@@ -11,7 +11,7 @@ using TelimenaClient;
 namespace Telimena.WebApp.Infrastructure.Database
 {
     //public class TelimenaDbInitializer : MigrateDatabaseToLatestVersion<TelimenaContext, Configuration>
-    public class TelimenaDbInitializer : DropCreateDatabaseAlways<TelimenaContext>
+    public class TelimenaDbInitializer : DropCreateDatabaseIfModelChanges<TelimenaContext>
     {
         public static void SeedUsers(TelimenaContext context)
         {
