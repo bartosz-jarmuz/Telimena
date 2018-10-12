c282cbd631a79d9affc78606ef14bd18723a1b7b
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-10-12 18:09:40 +0200

    Add migration

---
+++ Telimena/Telimena.WebApp.Infrastructure/Migrations/201810121605503_updaterRoundtripWip.cs
@@ -0,0 +1,16 @@
+namespace Telimena.WebApp.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updaterRoundtripWip : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
