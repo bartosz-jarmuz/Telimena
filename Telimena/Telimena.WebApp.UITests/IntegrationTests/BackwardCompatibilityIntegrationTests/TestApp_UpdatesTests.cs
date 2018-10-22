3751f126455aa79bada3d816747918251562b5cc
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-10-22 21:40:44 +0200

    start working towards update automtion

---
+++ Telimena/Telimena.WebApp.UITests/IntegrationTests/BackwardCompatibilityIntegrationTests/TestApp_UpdatesTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using AutomaticTestsClient;
+using NUnit.Framework;
+using Telimena.WebApp.UITests.Base;
+using TelimenaClient;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Telimena.WebApp.UITests.IntegrationTests.BackwardCompatibilityIntegrationTests
+{
+    [TestFixture()]
+    public partial class NonUiTests : PortalTestBase
+    {
+
+       
+        [Test]
+        public void HandleUpdatesNonBetaTests()
+        {
+            var result = this.LaunchTestsAppAndGetResult<UpdateCheckResult>(Actions.HandleUpdates);
+
+         
+        }
+
+
+
+      
+    }
+}
