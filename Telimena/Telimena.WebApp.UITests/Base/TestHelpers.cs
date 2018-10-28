090ca5427100cff15d27c060917920b0413b5456
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-10-28 22:30:23 +0100

    Add serialized expression to error

--- Telimena/Telimena.WebApp.UITests/Base/TestHelpers.cs
+++ Telimena/Telimena.WebApp.UITests/Base/TestHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using TestStack.White;
 using TestStack.White.Factory;
@@ -10,7 +11,7 @@ namespace Telimena.WebApp.UITests.Base
     public static class TestHelpers
     {
 
-        public static async Task<Window> WaitForWindowAsync(Predicate<string> match, TimeSpan timeout, string errorMessage = "")
+        public static async Task<Window> WaitForWindowAsync(Expression<Predicate<string>> match, TimeSpan timeout, string errorMessage = "")
         {
             Window win = null;
             Stopwatch timeoutWatch = Stopwatch.StartNew();
@@ -19,25 +20,27 @@ namespace Telimena.WebApp.UITests.Base
                 await Task.Delay(50);
 
                 Process[] allProcesses = Process.GetProcesses();
+                var compiled = match.Compile();
 
                 foreach (Process allProcess in allProcesses)
                 {
-                    if (!match.Invoke(allProcess.MainWindowTitle))
+                    if (!compiled.Invoke(allProcess.MainWindowTitle))
                     {
                         continue;
                     }
 
                     Application app = TestStack.White.Application.Attach(allProcess);
-                    win = app.Find(match, InitializeOption.NoCache);
+                    win = app.Find(compiled, InitializeOption.NoCache);
                     if (win != null)
                     {
                         return win;
                     }
                 }
-
                 if (timeoutWatch.Elapsed > timeout)
                 {
-                    throw new InvalidOperationException($"Failed to find window {errorMessage}");
+                    string expBody = ((LambdaExpression)match).Body.ToString();
+
+                    throw new InvalidOperationException($"Failed to find window by expression on Title: {expBody}.{errorMessage}");
                 }
             }
 
