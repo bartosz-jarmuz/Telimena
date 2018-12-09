40ed987e68c5abca84bcf797d5a21843baac09f7
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-12-09 16:33:18 +0100

    Restore tests

--- Telimena/Telimena.WebApp.UITests/02. IntegrationTests/BackwardCompatibilityIntegrationTests/TestApp_StatsTests.cs
+++ Telimena/Telimena.WebApp.UITests/02. IntegrationTests/BackwardCompatibilityIntegrationTests/TestApp_StatsTests.cs
@@ -13,44 +13,44 @@ namespace Telimena.WebApp.UITests._02._IntegrationTests.BackwardCompatibilityInt
     [TestFixture, Order(2)]
     public partial class _2_NonUiTests : IntegrationTestBase
     {
-        //[Test]
-        //public void InitializeTest()
-        //{
-        //    TelemetryInitializeResponse response = this.LaunchTestsAppAndGetResult< TelemetryInitializeResponse>(out _, Actions.Initialize, TestAppProvider.FileNames.TestAppV1, MethodBase.GetCurrentMethod().Name);
+        [Test]
+        public void InitializeTest()
+        {
+            TelemetryInitializeResponse response = this.LaunchTestsAppAndGetResult< TelemetryInitializeResponse>(out _, Actions.Initialize, TestAppProvider.FileNames.TestAppV1, MethodBase.GetCurrentMethod().Name);
 
-        //    Assert.IsNull(response.Exception);
-        //    Assert.IsTrue(response.UserId != Guid.Empty);
-        //    Assert.IsTrue(response.Count> 0);
+            Assert.IsNull(response.Exception);
+            Assert.IsTrue(response.UserId != Guid.Empty);
+            Assert.IsTrue(response.Count> 0);
 
-        //    TelemetryInitializeResponse responseNew = this.LaunchTestsAppAndGetResult<TelemetryInitializeResponse>(out _, Actions.Initialize, TestAppProvider.FileNames.TestAppV1, MethodBase.GetCurrentMethod().Name);
+            TelemetryInitializeResponse responseNew = this.LaunchTestsAppAndGetResult<TelemetryInitializeResponse>(out _, Actions.Initialize, TestAppProvider.FileNames.TestAppV1, MethodBase.GetCurrentMethod().Name);
 
-        //    Assert.AreEqual(responseNew.Count , response.Count +1);
-        //}
+            Assert.AreEqual(responseNew.Count , response.Count +1);
+        }
 
-        //[Test]
-        //public void ReportView()
-        //{
-        //    FileInfo app;
-        //    TelemetryUpdateResponse response = this.LaunchTestsAppAndGetResult<TelemetryUpdateResponse>(out app, Actions.ReportViewUsage, TestAppProvider.FileNames.TestAppV1, "", viewName: MethodBase.GetCurrentMethod().Name);
+        [Test]
+        public void ReportView()
+        {
+            FileInfo app;
+            TelemetryUpdateResponse response = this.LaunchTestsAppAndGetResult<TelemetryUpdateResponse>(out app, Actions.ReportViewUsage, TestAppProvider.FileNames.TestAppV1, "", viewName: MethodBase.GetCurrentMethod().Name);
 
-        //    Assert.IsNull(response.Exception);
-        //    Assert.IsTrue(response.TelemetryKey != Guid.Empty);
-        //    Assert.IsTrue(response.UserId != Guid.Empty);
-        //    Assert.IsTrue(response.Count > 0);
-        //    Assert.IsTrue(response.ComponentId > 0);
-        //    Assert.AreEqual("ReportView", response.ComponentName);
+            Assert.IsNull(response.Exception);
+            Assert.IsTrue(response.TelemetryKey != Guid.Empty);
+            Assert.IsTrue(response.UserId != Guid.Empty);
+            Assert.IsTrue(response.Count > 0);
+            Assert.IsTrue(response.ComponentId > 0);
+            Assert.AreEqual("ReportView", response.ComponentName);
 
-        //    TelemetryUpdateResponse responseNew = this.LaunchTestsAppAndGetResult<TelemetryUpdateResponse>(out app, Actions.ReportViewUsage, TestAppProvider.FileNames.TestAppV1, "", viewName: MethodBase.GetCurrentMethod().Name);
+            TelemetryUpdateResponse responseNew = this.LaunchTestsAppAndGetResult<TelemetryUpdateResponse>(out app, Actions.ReportViewUsage, TestAppProvider.FileNames.TestAppV1, "", viewName: MethodBase.GetCurrentMethod().Name);
 
-        //    Assert.AreEqual(responseNew.Count, response.Count + 1);
-        //    Assert.AreEqual("ReportView", responseNew.ComponentName);
+            Assert.AreEqual(responseNew.Count, response.Count + 1);
+            Assert.AreEqual("ReportView", responseNew.ComponentName);
 
-        //    TelemetryUpdateResponse customViewNameResponse = this.LaunchTestsAppAndGetResult<TelemetryUpdateResponse>(app, Actions.ReportViewUsage, viewName: "UnitTestView");
-        //    Assert.IsTrue(response.ComponentId < customViewNameResponse.ComponentId);
-        //    Assert.AreEqual("UnitTestView", customViewNameResponse.ComponentName);
-        //    Assert.IsTrue(customViewNameResponse.Count > 0);
+            TelemetryUpdateResponse customViewNameResponse = this.LaunchTestsAppAndGetResult<TelemetryUpdateResponse>(app, Actions.ReportViewUsage, viewName: "UnitTestView");
+            Assert.IsTrue(response.ComponentId < customViewNameResponse.ComponentId);
+            Assert.AreEqual("UnitTestView", customViewNameResponse.ComponentName);
+            Assert.IsTrue(customViewNameResponse.Count > 0);
 
-        //}
+        }
 
 
 
