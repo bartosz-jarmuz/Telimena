d6a2660dd8b17695662f7f94499b6c35e8ca53ae
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2019-02-05 17:32:06 +0100

    remove weird test and netstandard 20 dependency

--- Telimena/Telimena.Tests/MiscTests.cs
+++ Telimena/Telimena.Tests/MiscTests.cs
@@ -21,30 +21,6 @@ namespace Telimena.Tests
 
         }
 
-        [Test]
-        public void Main()
-        {
-            DoIt();
-        }
-
-        public void DoIt()
-        {
-            try
-            {
-                Trace.WriteLine("inner try");
-                int i = 0;
-                Trace.WriteLine(12 / i); // oops
-            }
-            catch (Exception e)
-            {
-                Trace.WriteLine("inner catch");
-                throw e; // or "throw", or "throw anything"
-            }
-            finally
-            {
-                Trace.WriteLine("inner finally");
-            }
-        }
 
     }
 }
