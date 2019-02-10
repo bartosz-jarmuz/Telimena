99e81aea6b14c88b2a011b9deb5d62854ce0a256
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2019-02-10 20:44:17 +0100

    Add sequence ID

---
+++ Telimena/Telimena.WebApp.Utils/SequenceIdParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telimena.WebApp.Utils
+{
+    public static class SequenceIdParser
+    {
+        public static string GetPrefix(string sequenceId)
+        {
+            return sequenceId.Remove(sequenceId.LastIndexOf(':'));
+        }
+
+        public static int GetOrder(string sequenceId)
+        {
+            var number = sequenceId.Substring(sequenceId.LastIndexOf(':')+1);
+
+            return Int32.Parse(number);
+        }
+    }
+}
